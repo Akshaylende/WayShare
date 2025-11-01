44c825ff163cb49365678122ab24c08cd32c3781
Akshay Lende <akshaylende48@gmail.com>
2025-11-02 03:45:35 +0530

    Integrating the registry logic with the frontend

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,6 @@
-from flask import app, render_template, redirect, url_for, request
+from flask import app, render_template, redirect, url_for, request, jsonify
 from app import app, db 
-from app.helpers import create_user
+from app.helpers import create_user, user_exists
 
 @app.route('/')
 def home():
@@ -18,15 +18,20 @@ def register():
     '''
     This function handles the registry of the new user on the platform 
     '''
+    data = request.get_json()
+    uname = data.get('username')
+    email = data.get('email')
+    pswd = data.get('password')
 
-    new_user = create_user('test1', 'test1@gmail.com', 'test1')
+    new_user = create_user(uname, email, pswd)
     if new_user is None:
-        return "User already exists"
+        return jsonify({"status": "error", 
+                        "message": "User with username already exists"}), 400
     else:
         db['registry'].append(new_user)
     
-    redirect(url_for('home'))
-
+    return jsonify({"status": "success",
+                    "message": "New User Created Successfully"}), 200
 
 @app.route('/sign-in',  methods = ['GET'])
 def signin():
@@ -37,8 +42,12 @@ def login():
     '''
     This function checks for the user in our db and checks for validity
     '''
-    user = request.data.json()
-    if user in db["registry"]:
-        redirect(url_for('home'))
+    user = request.get_json()
+    email = user['email']
+    pwsd  = user['password']
+    if not user_exists(email, pwsd):
+        return jsonify({"status": "error",
+                        "message":"Invalid Credentials" }), 400
     
-    redirect(url_for('signin'))
+    return jsonify({"status": "success",
+                    "message": "Welcome back! {uname}"}), 200
