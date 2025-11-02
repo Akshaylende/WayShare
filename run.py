3fe93a82faa93da5c9c0ebeaaa12af86971c0bbe
Akshay Lende <akshaylende48@gmail.com>
2025-11-02 16:21:50 +0530

    Change server port from 5001 to 5050

--- run.py
+++ run.py
@@ -8,5 +8,5 @@ app = create_app()
 # This is the main entry point to run the application server
 if __name__ == "__main__":
     # The debug=True flag enables auto-reloading when code changes
-    # The port is set to 5001 as in the original file
-    app.run(debug = True, port = 5001)
+    # The port is set to 5050 as in the original file
+    app.run(debug = True, port = 5050)
