65fbf8fe57176dc9cb34eaf1e194f1cafffbae49
Akshay Lende <akshaylende48@gmail.com>
2025-11-04 01:49:35 +0530

    Connection for the mongodb Atlas

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,7 @@
 from flask import Flask
+from mongoengine import connect
 
 
-db = {}
 app = Flask(__name__)
 
 def create_app():
@@ -10,8 +10,14 @@ def create_app():
     This function creates and configures the Flask application instance.
     '''
     # Initializing user collection 
-    db['registry'] = [{'username': 'test', 'email': 'test@gmail.com', 'password': 'test'}]
+    # db['registry'] = [{'username': 'test', 'email': 'test@gmail.com', 'password': 'test'}]
+    app.secret_key = 'f633126a21c0aae16266af82ee0772b5947f58d2f3236aa9f7f18a59a2a80742'
     
+    connect(
+        db = "wayshare_db",
+        host = "mongodb+srv://akshaylende2408_db_user:HMIbHKzx97BViQhB@cluster0.a2namms.mongodb.net/" 
+    )
+
     # Importing routes
     from app import routes
     
