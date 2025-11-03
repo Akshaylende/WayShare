9c4eb5dce50789552f9f2d2290e0806e62960557
Akshay Lende <akshaylende48@gmail.com>
2025-11-04 01:52:14 +0530

    Updating our helper function as per the db requirements

--- app/helpers.py
+++ app/helpers.py
@@ -1,34 +1,46 @@
-from app import app, db
-
-def Initialise_dB() -> None:
-    db['registry'] = []
-
-def create_user(uname, email, pswd):
-    '''
-    This function creates a new user ensuring the unqiue user details
-    '''
-    if check_valid_user(uname, email) is False:
-        return None
-
-    user = {}
-    user["userName"] = uname
-    user["email"] = email
-    user["password"] = pswd
-
-    return user
+from app import app
+from app.models import Registry, User
 
 
-def check_valid_user(uname, email) -> bool:
 
-    for user in db['registry']:
-        if user['userName'] == uname or user['email'] == email:
-            return False 
+# def Initialise_dB() -> None:
+#     db['registry'] = []
+
+# def create_user(uname, email, pswd):
+#     '''
+#     This function creates a new user ensuring the unqiue user details
+#     '''
+#     if check_valid_user(uname, email) is False:
+#         return None
+
+#     user = {}
+#     user["username"] = uname
+#     user["email"] = email
+#     user["password"] = pswd
+
+#     return user
+
+
+# def check_valid_user(uname, email) -> bool:
+
+#     for user in db['registry']:
+#         if user['username'] == uname or user['email'] == email:
+#             return False 
     
-    return True
+#     return True
 
-def user_exists(email, pwsd)->bool:
-    for user in db['registry']:
-        if user['email'] == email and user['password'] == pwsd:
-            return True
-        
+def user_exists(email, uname)->bool:
+    
+    user = Registry.objects(email = email).first()
+    if user:
+        return True
+    user = Registry.objects(username = uname).first()
+    if user:
+        return True
+    return False
+
+def check_user_cred(email, pswd):
+    user = Registry.objects(email = email, password = pswd).first()
+    if user:
+        return True
     return False
